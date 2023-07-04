d9b53aeebbafad8ab2db265b3fb40d293aa264bb
Mohmoduol Hasan Sabbir <60088050+EngHasanSabbir@users.noreply.github.com>
2023-07-04 11:45:07 +0600

    Checking All Variable Type

--- 5.cpp
+++ 5.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
 
-cout<<sizeof(int);
+cout<<sizeof(int)<<endl;
+cout<<sizeof(float)<<endl;
+cout<<sizeof(char)<<endl;
+cout<<sizeof(double)<<endl;
+cout<<sizeof(long)<<endl;
+cout<<sizeof(bool)<<endl;
+cout<<sizeof(long long)<<endl;
+cout<<sizeof(short)<<endl;
 
 cout<<endl<<"Code Running Successfull";
 return 0;
