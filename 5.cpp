54db38866badb7389f65eacb41836b4a9950effe
Mohmoduol Hasan Sabbir <60088050+EngHasanSabbir@users.noreply.github.com>
2023-07-04 11:47:08 +0600

    Add Succefull

--- 5.cpp
+++ 5.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -15,3 +16,5 @@ cout<<sizeof(short)<<endl;
 cout<<endl<<"Code Running Successfull";
 return 0;
 }
+=======
+>>>>>>> parent of 4405af1 (Only Int Checking)
