e530e618bd4dbae60bc211c36d98b6446803ea4a
Mohmoduol Hasan Sabbir <60088050+EngHasanSabbir@users.noreply.github.com>
2023-07-04 12:09:04 +0600

    Final 7

---
+++ 7.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Check(int a){
+
+(a==0)? cout<<"Even":cout<<"Odd";
+
+}
+
+int main(){
+    int a;
+    cout<<"Enter a Number to check Even or Odd: ";
+    cin>>a;
+    Check(a);
+
+}
