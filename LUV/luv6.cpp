aa5a54ab8782e3a8602491c9bc5d4e8af4c03a1b
Mohmoduol Hasan Sabbir <60088050+EngHasanSabbir@users.noreply.github.com>
2023-07-07 11:24:05 +0600

    v1

---
+++ LUV/luv6.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+string name="Hasan Sabbir";
+string roll="2 0";
+
+string result=name+roll;
+cout<<result;
+
+if(name==roll){
+
+    cout<<"Equal";
+}
+else {
+    cout<<"\n Not Equal";
+}
+
+    return 0;
+}
